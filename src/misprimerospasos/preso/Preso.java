9412d546c71817d15da05eee54ae3b41b5460914
Luisa Caviedes <lcaviedesrestrepo@gmail.com>
2017-07-13 21:11:59 -0500

    Paquete Presos, patron singleton presos

---
+++ src/misprimerospasos/preso/Preso.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package misprimerospasos.preso;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author User
+ */
+public class Preso {
+    public String nombres;
+    public String apellidos;
+    public String identificacion;
+    private final ArrayList<String> presos = new ArrayList<>();
+    private static Preso instancia;
+    
+    public Preso(){
+        
+    }
+    
+    public static Preso getPreso(){
+        if(instancia == null){
+            instancia = new Preso();
+        }
+        return instancia;
+    }
+    
+    public void crearPreso(){
+    }
+}
