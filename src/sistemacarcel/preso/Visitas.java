7c16a28a684dd5a2c81dc2407d4e759d91d812d5
Luisa Caviedes <lcaviedesrestrepo@gmail.com>
2017-07-20 19:36:18 -0500

    Visitas

--- src/sistemacarcel/preso/Visitas.java
+++ src/sistemacarcel/preso/Visitas.java
@@ -14,11 +14,30 @@ import java.util.Date;
  * @author User
  */
 public class Visitas {
-    public Date fechaVisita;
-    public Time horaVisita;
-    public ArrayList<String> visitas = new ArrayList<>();
+    protected Date fechaVisita;
+    protected Time horaVisita;
+    protected int  idVisitante;
+    protected int  idPreso;
     
-    public void registrarVisita(){
+    public ArrayList<Visitas> visitas = new ArrayList<>();
+    
+    /**
+     *
+     * @param fechaVisita
+     * @param horaVisita
+     * @param idPreso
+     * @param idVisitante
+     */
+    public void Visitas(Date fechaVisita,Time horaVisita, int idPreso, int idVisitante){
+        this.fechaVisita = fechaVisita;
+        this.horaVisita  = horaVisita;
+        this.idPreso     = idPreso;
+        this.idVisitante = idVisitante;
+    }
+    
+    public void registrarVisita(Visitas visita){
+        visitas.add(visita);
+        System.out.println("Registro visita");
     }
     
 }
